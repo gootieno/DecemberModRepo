eedfcb50438e190ab7f2cb2a0948695b48225323
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-02-15 13:25:49 -0800

    fetch short practice solution

--- Week8/Day3/solutions/04-fetch/fetch-requests.js
+++ Week8/Day3/solutions/04-fetch/fetch-requests.js
@@ -6,20 +6,40 @@ Paste your code for fetch requests here once you finish each task.
 /* =============== 1. Print the status code of the response =============== */
 
 // Your code here
+const getProducts = async () => {
+  //   const response = await fetch("/products");
+  //   console.log("response ", response);
 
+  //   console.log("response status ", response.status);
+  //   console.log("response ok? ", response.ok);
+  //   console.log("response headers ", response.headers.get("content-type"));
+  //   //   console.log("response text ", await response.text());
+  //   const data = await response.text();
+  //   console.log("response text data ", data);
 
+  // using .then()
+
+  return await fetch("/products")
+    .then((response) => {
+      console.log("response status ", response.status);
+      console.log("response ok? ", response.ok);
+      console.log("response headers ", response.headers.get("content-type"));
+      return response.text();
+    })
+    .then((data) => {
+      console.log("response data ", data);
+    });
+};
+
+getProducts();
 
 /* ====== 2. Print true if the status of the response was successful ====== */
 
 // Your code here
 
-
-
 /* =================== 3. Print the Content-Type Header =================== */
 
 // Your code here
-
-
 
 /* ============== 4. Print the body of the response as text =============== */
 
