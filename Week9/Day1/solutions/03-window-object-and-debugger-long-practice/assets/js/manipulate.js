9453b18943cd636897aacfad3ff65a451e2500c4
Tanner Shaw <traumaaa@gmail.com>
2023-02-20 19:01:42 -0600

    Long practice solutions

--- Week9/Day1/solutions/03-window-object-and-debugger-long-practice/assets/js/manipulate.js
+++ Week9/Day1/solutions/03-window-object-and-debugger-long-practice/assets/js/manipulate.js
@@ -2,17 +2,24 @@ function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
 
     // Your code here
+    window.document.title = "My Portfolio";
 }
 
 function changeHeader() {
     // Change the name in the h1 of the page to your name
-
+    const body = document.body;
+    const header = body.children[0]
+    const h1 = header.children[0]
+    h1.innerText = "New header!";
     // Your code here
 }
 
 function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
-
+    const body = document.body;
+    const aboutMe = body.children[1];
+    const firstParagraph = aboutMe.children[1];
+    firstParagraph.innerText = "Insert some cool facts about yourself here";
     // Your code here
 }
--- Week9/Day1/solutions/03-window-object-and-debugger-long-practice/assets/js/search.js
+++ Week9/Day1/solutions/03-window-object-and-debugger-long-practice/assets/js/search.js
@@ -1,29 +1,86 @@
-function findElementById(id) {
+function findFirstMatchingElement(parentEle, cb) {
+    // do a depth-first search to recursively find the first element in the tree
+    // that will return true when passed into the cb function
+    if (cb(parentEle)) {
+        return parentEle;
+    }
+
+    for (let i = 0; i < parentEle.children.length; i++) {
+        const result = findFirstMatchingElement(parentEle.children[i], cb);
+        if (result) return result;
+    }
+
+    return null;
+}
+
+function findAllMatchingElements(parentEle, cb) {
+    // recursively find all elements in the tree that will return true when
+    // passed into the cb function (depth-first search)
+    let results = [];
+    if (cb(parentEle)) {
+        results.push(parentEle);
+    }
+
+    for (let i = 0; i < parentEle.children.length; i++) {
+        const childResults = findAllMatchingElements(parentEle.children[i], cb);
+        if (childResults.length) results = results.concat(childResults);;
+    }
+
+    return results;
+}
+
+
+export function findElementById(id) {
+    //!!END
     // Return the element in the DOM with corresponding `id`
 
-    // Your code here
+    //!!START
+    return findFirstMatchingElement(document.body, function (ele) {
+        return ele.id === id;
+    });
+    //!!END
 }
 
-function findFirstElementOfTag(tag) {
+export function findFirstElementOfTag(tag) {
+    //!!END
     // Return the first occurence of an element of tag name `tag`
 
-    // Your code here
+    //!!START
+    return findFirstMatchingElement(document.body, function (ele) {
+        return ele.tagName === tag;
+    });
+    //!!END
 }
 
-function findFirstElementOfClass(cls) {
+export function findFirstElementOfClass(cls) {
+    //!!END
     // Return the first occurence of an element of class `cls`
 
-    // Your code here
+    //!!START
+    return findFirstMatchingElement(document.body, function (ele) {
+        return ele.classList.contains(cls);
+    });
+    //!!END
 }
 
-function findElementsOfTag(tag) {
+export function findElementsOfTag(tag) {
+    //!!END
     // Return an array of elements that have a tag name of `tag`
 
-    // Your code here
+    //!!START
+    return findAllMatchingElements(document.body, function (ele) {
+        return ele.tagName === tag;
+    });
+    //!!END
 }
 
-function findElementsOfClass(cls) {
+export function findElementsOfClass(cls) {
+    //!!END
     // Return an array of elements that have are of class `cls`
 
-    // Your code here
+    //!!START
+    return findAllMatchingElements(document.body, function (ele) {
+        return ele.classList.contains(cls);
+    });
+    //!!END
 }
