2cbfdd1a3b5b120042738bcda9bc86fd394ea1f4
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-02-22 11:14:48 -0800

    data attributes solution

--- Week9/Day3/solutions/02-data-on-elements/data.js
+++ Week9/Day3/solutions/02-data-on-elements/data.js
@@ -1,1 +1,23 @@
 // Your code here
+window.addEventListener("DOMContentLoaded", () => {
+  const addButton = document.getElementById("add");
+
+  addButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const shoppingList = document.getElementById("shopping-list");
+
+    const type = document.getElementById("type");
+    const name = document.getElementById("name");
+
+    const listItem = document.createElement("li");
+
+    // listItem.setAttribute("data-type", type.value);
+    listItem.dataset.type = type.value;
+
+    listItem.innerText = name.value;
+
+    shoppingList.append(listItem);
+    name.value = "";
+  });
+});
